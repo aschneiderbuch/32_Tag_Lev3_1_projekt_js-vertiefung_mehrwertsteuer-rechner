64c90aa4675c0ec4c40a9aef812697b0aa1d4e11
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-26 14:03:49 +0100

    htmlGeruestUndRadioNurEinenAuswaelen

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/* 
+
+Frage
+
+Auswahl 2 Radio buttons für Rechenweg
+    1. Netto zu Brutto
+    2. Brutto zu Netto
+
+dann 2 Readio buttons für Steuer 
+    1. 19%
+    2. 17%
+
+dann Input feld für den Wert
+
+dann Button berechnen
+
+
+dann output Ergebnis in
+    Mehrwertsteuerbetrag ... in Euro
+    Betrag 
+         Bruttobetrag (Endpreis ) ... in Euro
+         oder 
+         Nettobetrag ... in Euro        // hier kommt ja eigentlich noch die Steuer dazu
+
+*/
